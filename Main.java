0d34e27131bda762902491843e8fc74ac9393719
PEBBLESLAPTOP\pauld <pauldefouc@gmail.com>
2025-11-05 10:04:48 +0100

    main editado por pablo (línea 5)

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Git");
         System.out.println("Editado");
+        System.out.println("Editado por segunda vez por Pablo");
 
     }
 }
